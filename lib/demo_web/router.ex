6f1bc3ab4c96826e13aab538e032799a802df0a8
Alvise Susmel <alvise@poeticoding.com>
2019-06-18 16:16:13 +0100

    pictures without pushState support

--- lib/demo_web/router.ex
+++ lib/demo_web/router.ex
@@ -20,6 +20,7 @@ defmodule DemoWeb.Router do
 
     get "/", PageController, :index
 
+    live "/pictures", PicturesLive
     live "/thermostat", ThermostatLive
     get "/snake", PageController, :snake
     live "/search", SearchLive
